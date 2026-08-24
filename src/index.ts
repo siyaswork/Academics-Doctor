c0e7b7e508632c5a798a80fde5f4d3308fb9165f
siyaswork <sssiya053@gmail.com>
2026-08-24 10:17:07 +0200

    Add component interfaces, layout components, and foundation guide

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+// Components
+export { Button } from './Button'
+export type { ButtonProps } from './Button.interface'
+
+export { Card } from './Card'
+export type { CardProps } from './Card.interface'
+
+export { Input } from './Input'
+export type { InputProps } from './Input.interface'
+
+export { Select } from './Select'
+export type { SelectProps } from './Select.interface'
+
+export { Badge } from './Badge'
+export type { BadgeProps } from './Badge.interface'
+
+export { Tabs } from './Tabs'
+export type { TabsProps } from './Tabs.interface'
+
+export { Modal } from './Modal'
+export type { ModalProps } from './Modal.interface'
+
+export { Alert } from './Alert'
+export type { AlertProps } from './Alert.interface'
+
+export { Spinner } from './Spinner'
+export type { SpinnerProps } from './Spinner.interface'
+
+export { ThemeToggle } from './ThemeToggle'
+
+// Layouts
+export { MainLayout } from '../layouts/MainLayout'
+export type { MainLayoutProps } from '../layouts/MainLayout.interface'
+
+// Contexts
+export { ThemeProvider, useTheme } from '../contexts/ThemeContext'
+export type { ThemeContextType } from '../contexts/ThemeContext'
